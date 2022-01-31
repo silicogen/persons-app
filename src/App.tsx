3010e893fefd54409405f8e43431819730bab76d
alex <alexey.chuprov@gmail.com>
2022-01-31 14:01:08 +0300

    clear

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,12 @@ import { Persons } from './features/persons/Persons';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <main>
-        <header className="App-main">
-          <Persons />
-          <Counter />
-        </header>
-      </main>
-    </div>
-  );
+  return <div className="App">
+    <main className="App-main">
+      <Persons />
+      <Counter />
+    </main>
+  </div>
 }
 
 export default App;
